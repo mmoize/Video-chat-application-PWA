1f2b3da417f1818d669a7c7725c8ab54961a5da5
Moise <murhi46@gmail.com>
2021-09-26 23:29:11 +1000

    update for keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyCNNFmjr9xLuFajpg-5TtUqVfXXh-7WwZk",
-    authDomain: "video-chat-app-pwa.firebaseapp.com",
-    databaseURL: "https://video-chat-app-pwa-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "video-chat-app-pwa",
-    storageBucket: "video-chat-app-pwa.appspot.com",
-    messagingSenderId: "555873223113",
-    appId: "1:555873223113:web:fb3fc979026ccc43306818",
-    measurementId: "G-1DZBS134BB"
-  }
+
 
 };
 
